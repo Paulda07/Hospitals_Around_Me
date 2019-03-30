c28879fbd356b55a297f38fe7f8fbd9a92292559
Paulda07 <40849148+Paulda07@users.noreply.github.com>
2019-03-30 13:20:11 +0530

    Linking the results page

--- Finding_Hospitals/find_hospital/models.py
+++ Finding_Hospitals/find_hospital/models.py
@@ -13,13 +13,14 @@ from django.db import models
 
 class Find_Hosp(models.Model):
 
-    title = models.CharField(max_length=200)
+	latitude = models.FloatField(default = 0)
+	longitude = models.FloatField(default = 0)
+	name = models.CharField(max_length=200)
+	address = models.TextField()
+	contact = models.CharField(max_length= 20)
 
-    text = models.TextField()
 
+	def __str__(self):
 
-
-    def __str__(self):
-
-        return self.title
+		return self.name
     
